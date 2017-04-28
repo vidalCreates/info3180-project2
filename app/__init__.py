fe6688c1c2cfaab3062bdf3092a9f1c3832f5dd6
vidalCreates <amc12vidal@hotmail.com>
2017-04-28 11:33:51 -0500

    updated sqlalchemy uri

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_jwt import JWT
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "this is a random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project2:project2@localhost/project2"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fshmbfalghrtee:d23e3708771485cf521f437dc2d9aa4380bc6ed02ddb17e4f19e2e4ca676c05c@ec2-54-227-237-223.compute-1.amazonaws.com:5432/d4hkdh467pa9aa'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 # UPLOAD FOLDER
