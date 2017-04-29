b3a54c1199cbaf8e7a03614ba282960ca763b16e
vidalCreates <amc12vidal@hotmail.com>
2017-04-29 16:49:48 -0500

    push to heroku attempt 2

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_jwt import JWT
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "this is a random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fshmbfalghrtee:d23e3708771485cf521f437dc2d9aa4380bc6ed02ddb17e4f19e2e4ca676c05c@ec2-54-227-237-223.compute-1.amazonaws.com:5432/d4hkdh467pa9aa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://joxkojdnhhkixw:a95d095deb5b242f0088b39220c0937afa065aa77e8279c0bd3ad2beed481003@ec2-50-19-95-47.compute-1.amazonaws.com:5432/d1ebjlcv1ajonp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 # UPLOAD FOLDER
