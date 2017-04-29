8424607bc4329c5aba390bf8b516278128169cd8
vidalCreates <amc12vidal@hotmail.com>
2017-04-29 17:56:30 -0500

    change uri back to local

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_jwt import JWT
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "this is a random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://joxkojdnhhkixw:a95d095deb5b242f0088b39220c0937afa065aa77e8279c0bd3ad2beed481003@ec2-50-19-95-47.compute-1.amazonaws.com:5432/d1ebjlcv1ajonp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://project2:project2@localhost/project2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 # UPLOAD FOLDER
